76412c6621cfe7c5071ee319688adb6844d6ce42
Pulkit Thakar <pulkit.thakar@gmail.com>
2023-10-26 00:27:56 -0500

    Working on importing data

--- gradebook.py
+++ gradebook.py
@@ -1,2 +1,37 @@
 import pandas as pd
- 
+import os
+import glob
+
+# reading the roster.csv file
+rosterDF = pd.read_csv(
+    "data/roster.csv",
+    converters={
+        'Email Address': str.lower,
+        'NetID': str.lower
+    },
+)
+
+hwAndExamsDF = pd.read_csv(
+    "data/hw_exam_grades.csv",
+    converters={
+        'SID': str.lower,
+    },
+    usecols= lambda x: "Submission" not in x
+)
+
+quizDF = pd.DataFrame()
+for file_path in glob.glob(os.getcwd() + "/data/quiz_*_grades.csv"):
+    quizName = "Quiz" + file_path.split("_")[1]
+    quiz = pd.read_csv(
+        file_path,
+        converters={
+            "Email": str.lower
+        },
+        index_col=["Email"],
+        usecols=["Email", "Grade"]
+    ).rename(columns={"Grade": quizName})
+    quizDF = pd.concat([quizDF, quiz], axis=1)
+    print(quizDF)
+    
+
+# print(quizDF)
